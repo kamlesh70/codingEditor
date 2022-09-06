a2eb7ede940910aa8a3d7c0962252b2d29195057
kamlesh mehra <kamalmehra3571@gmail.com>
2022-09-06 19:41:07 +0530

    add Front-end part

---
+++ src/Pages/Home.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { v4 as uuidV4 } from "uuid";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const navigate = useNavigate();
+  const [roomId, setRoomId] = useState("");
+  const [userName, setUserName] = useState("");
+  const onCreateRoomID = (e) => {
+    e.preventDefault();
+    let id = uuidV4();
+    setRoomId(id);
+    toast.success("created a new room");
+  };
+  const onJoinHandler = (e) => {
+    if (roomId.trim() == "" || userName.trim() == "") {
+      toast.error("room id & user name required");
+      return;
+    }
+    navigate(`/editor/${roomId}`, {
+      state: {
+        userName: userName,
+      },
+    });
+  };
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img src="log.png" alt="Code Editor logo" />
+        <h4 className="mainLabel">Past Invitation Room ID</h4>
+        <div className="inputGroup">
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="ROOM ID"
+            value={roomId}
+            onChange={(e) => setRoomId(e.target.value)}
+          />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="USERNAME"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <button className="btn joinBtn" onClick={onJoinHandler}>
+            Join
+          </button>
+          <span className="createInfo">
+            if you don't have an invite then create &nbsp;
+            <a className="createNewBtn" href="" onClick={onCreateRoomID}>
+              new room
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
